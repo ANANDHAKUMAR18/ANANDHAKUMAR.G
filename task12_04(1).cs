0ec42d84132db3f8772d911367d5c8e97bb72b11
ANANDHAKUMAR18 <101105244+ANANDHAKUMAR18@users.noreply.github.com>
2022-04-12 17:09:47 +0530

    Add files via upload

---
+++ task12_04(1).cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace data
+{
+    class Class6
+    {
+       public static void Main()
+        {
+
+            // SqlConnection con = new SqlConnection("Data source =DESKTOP-7MGGRB4; database =Anand; user id = sa;password = P@ssword");
+            //  string sqlqry = "create table enroll(sno int,Fname varchar(20),Lname varchar(20),Gender varchar(20));
+            //  SqlCommand cmd = new SqlCommand(sqlqry, con);
+            //  con.Open();
+            //  cmd.ExecuteNonQuery();
+            //  con.Close();
+            //  Console.WriteLine("created successfully");
+
+
+
+            //  SqlConnection con = new SqlConnection("Data source =DESKTOP-7MGGRB4; database =Anand; user id = sa;password = P@ssword");
+            //  string sqlqry = "insert enroll values(1001,'Anand','kumar','M')";
+            // SqlCommand cmd = new SqlCommand(sqlqry, con);
+            // con.Open();
+            //cmd.ExecuteNonQuery();
+            //  con.Close();
+            // Console.WriteLine("Row Inserted");
+
+
+
+
+            // SqlConnection con = new SqlConnection("Data source =DESKTOP-7MGGRB4; database =Anand; user id = sa;password = P@ssword");
+            //  string sqlqry = "delete from enroll where  Fname='Anand'";
+            //  SqlCommand cmd = new SqlCommand(sqlqry, con);
+            //  con.Open();
+            //  cmd.ExecuteNonQuery();
+            //  con.Close();
+            //  Console.WriteLine("Deleted is done");
+
+
+
+            // SqlConnection con = new SqlConnection("Data source =DESKTOP-7MGGRB4; database =Anand; user id = sa;password = P@ssword");
+            //string sqlqry = "update enroll Set Fname='Thangam' Lname='Kar' where Gender='F'";
+            // SqlCommand cmd= new SqlCommand("update tbl_enrollment Set Fname='Thangam' Lname='Kar' where Gender='F'", con);
+            // con.Open();
+           // cmd.ExecuteNonQuery();
+            // con.Close();
+            //Console.WriteLine("Updated is done");
+
+
+
+        }
+
+    }
+}
---
+++ task12_04(2).cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace data
+{
+    class class7
+    {
+        SqlConnection con = new SqlConnection("data source = DESKTOP-7MGGRB4;database = Anand;user id = sa ;password = P@ssword");
+        public void createtable()
+        {
+            SqlConnection con = new SqlConnection("data source =DESKTOP-7MGGRB4;database = Anand;user id = sa ;password = P@ssword");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("create table newTaskTest (sno int, StuName varchar(20))", con);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Completed Query");
+            con.Close();
+        }
+        public void insertmeth(int sno, string StuName)
+        {
+            SqlConnection con = new SqlConnection("data source = DESKTOP-7MGGRB4;database = Anand;user id = sa ;password = P@ssword");
+            SqlCommand cmd = new SqlCommand("sp_insTask", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add("@sno", SqlDbType.Int).Value = sno;
+            cmd.Parameters.Add("@StuName", SqlDbType.VarChar).Value = StuName;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void Updatemeth(int sno, string StuName)
+        {
+
+            SqlConnection con = new SqlConnection("data source =DESKTOP-7MGGRB4;database = Anand;user id = sa ;password = P@ssword");
+            SqlCommand cmd = new SqlCommand("upd_task", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add("@sno", SqlDbType.Int).Value = sno;
+            cmd.Parameters.Add("@StuName", SqlDbType.VarChar).Value = StuName;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public void deletemeth(int sno)
+        {
+            SqlConnection con = new SqlConnection("data source =DESKTOP-7MGGRB4;database = Anand;user id = sa ;password = P@ssword");
+            SqlCommand cmd = new SqlCommand("del_task", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add("@sno", SqlDbType.Int).Value = sno;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+        public static void Main()
+        {
+            class7 nobj = new class7();
+            nobj.createtable();
+            //Console.WriteLine("Press any Key to Continue....");
+            //Console.ReadLine();
+            //Console.WriteLine("Enter sno");
+            //int sn = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter a topic");
+            //string tpic = Console.ReadLine();
+            //nobj.insertmeth(sn, tpic);
+            //nobj.Updatemeth(sn,tpic);
+            //nobj.deletemeth(sn);
+
+        }
+    }
+}
