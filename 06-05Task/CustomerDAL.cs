baec5d6c7ae328789df0a0c894c2d7e3d9579b8b
ANANDHAKUMAR18 <101105244+ANANDHAKUMAR18@users.noreply.github.com>
2022-05-06 09:48:40 +0530

    Add files via upload

---
+++ 06-05Task/CustomerDAL.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using ADONet.Models;
+using System.Data;
+using System.IO;
+
+namespace ADONet.DAL
+{
+    public class CustomerDAL
+    {
+        public string cnn = "";
+
+
+        public CustomerDAL()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath
+                (Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            cnn = builder.GetSection("ConnectionStrings:Conn").Value;
+        }
+
+        public List<Customer> GetCustomerByID(int CustomerID)
+        {
+            List<Customer> listCustomer = new List<Customer>();
+            using (SqlConnection con = new SqlConnection(cnn))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetCustomerByID", con))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    cmd.Parameters.Add("@id", SqlDbType.Int);
+                    cmd.Parameters["@id"].Value = CustomerID;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    IDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        listCustomer.Add(new Customer()
+                        {
+                            CustomerID = int.Parse(reader["CustomerID"].ToString()),
+                            CustomerName = reader["CustomerName"].ToString(),
+                            EmailID = reader["EmailID"].ToString(),
+                            MobileNo = reader["MobileNo"].ToString()
+                        }); ;
+                    }
+
+                }
+            }
+            return listCustomer;
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            List<Customer> listCustomer = new List<Customer>();
+            using (SqlConnection con = new SqlConnection(cnn))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetAllCustomers", con))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    IDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        listCustomer.Add(new Customer()
+                        {
+                            CustomerID = int.Parse(reader["CustomerID"].ToString()),
+                            CustomerName = reader["CustomerName"].ToString(),
+                            EmailID = reader["EmailID"].ToString(),
+                            MobileNo = reader["MobileNo"].ToString()
+                        }); ;
+                    }
+
+                }
+            }
+            return listCustomer;
+        }
+        public int NewCustomer(Customer cobj)
+        {
+            SqlConnection con = new SqlConnection(cnn);
+            SqlCommand cmd = new SqlCommand("NewCustomer", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@cname", cobj.CustomerName);
+            cmd.Parameters.AddWithValue("@email", cobj.EmailID);
+            cmd.Parameters.AddWithValue("@mobno", cobj.MobileNo);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+
+
+
+        }
+        public int DelCustomer(Customer obj)
+        {
+            SqlConnection con = new SqlConnection(cnn);
+            SqlCommand cmd = new SqlCommand("DelCustomer", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", obj.CustomerID);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public int UpdCustomer(Customer cobj)
+        {
+            SqlConnection con = new SqlConnection(cnn);
+            SqlCommand cmd = new SqlCommand("UpdCustomer", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", cobj.CustomerID);
+            cmd.Parameters.AddWithValue("@cname", cobj.CustomerName);
+            cmd.Parameters.AddWithValue("@email", cobj.EmailID);
+            cmd.Parameters.AddWithValue("@mobno", cobj.MobileNo);
+            con.Open();
+            int result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        
+    }
+}
---
+++ 06-05Task/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using ADONet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ADONet.DAL;
+
+namespace ADONet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            CustomerDAL custDAL = new CustomerDAL();
+            List<Customer> CustomerList = new List<Customer>();
+            CustomerList = custDAL.GetAllCustomers();
+            return View(CustomerList);
+        }
+        [HttpPost]
+        public IActionResult Insert(Customer cs)
+        {
+            CustomerDAL cobj = new CustomerDAL();
+            int result = cobj.NewCustomer(cs);
+            if (result == 1)
+            {
+                return RedirectToAction("Index");
+
+            }
+            else
+                return RedirectToAction("Create");
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+      [HttpGet]
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Del(Customer c)
+        {
+            CustomerDAL cobj = new CustomerDAL();
+            int result = cobj.DelCustomer(c);
+            if (result == 1)
+            {
+                return RedirectToAction("Index");
+            }
+            else return RedirectToAction("Delete");
+        }
+        
+        public IActionResult Update()
+        {
+            return View();
+        }
+        
+        public IActionResult Upd(Customer c)
+        {
+            CustomerDAL cobj = new CustomerDAL();
+            int result = cobj.UpdCustomer(c);
+            if (result == 1)
+            {
+                return RedirectToAction("Index");
+            }
+            else return RedirectToAction("Update");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult SearchCustomer(int CustomerID)
+        {
+            CustomerDAL custDAL = new CustomerDAL();
+            List<Customer> CustomerList = new List<Customer>();
+            CustomerList = custDAL.GetCustomerByID(CustomerID);
+            return View(CustomerList);
+        }
+     
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
